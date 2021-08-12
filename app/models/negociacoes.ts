644c1f5cefef7c45940eaee37b88007f8aa3b83c
Alefros <alef.r.oliveira@gmail.com>
2021-08-12 00:29:11 -0300

    Add comments to others implementation's options

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,12 +1,15 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
+
+    //private negociacoes: Negociacao[] = [] <--- other option of implementation;
     private negociacoes: Array<Negociacao> = [];
 
     adiciona(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao);
     }
 
+    //lista(): readonly Negociacao[] {  <--- other option of implementation
     lista(): ReadonlyArray<Negociacao> {
         return this.negociacoes;
     }

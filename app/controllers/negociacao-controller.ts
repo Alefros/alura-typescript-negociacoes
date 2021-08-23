1c1c0234b543227c0fd1e8f658e494dde4642fa4
Alefros <alef.r.oliveira@gmail.com>
2021-08-23 00:08:55 -0300

    Change to use static method to create Negociacao

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -20,20 +20,16 @@ export class NegociacaoController {
     }
 
     adiciona(): void {
-      
-        const negociacao = this.criaNegociacao();
+        
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
         this.mensagemView.update('Negociação adicionada com sucesso');
         this.limparFormulario();
-    }
-
-    criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const data = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value); 
-        return new Negociacao(data, quantidade, valor);
     }
 
     limparFormulario(): void {

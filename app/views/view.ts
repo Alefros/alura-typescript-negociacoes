f604ee5d218c51334c30c5644a62dbd95908a8c5
Alefros <alef.r.oliveira@gmail.com>
2021-08-23 00:22:23 -0300

    Add validation to remove tag script of the template

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,13 +1,19 @@
 export abstract class View<T> {
     
     protected elemento: HTMLLIElement;
+    private escapar: boolean = false;
 
-    constructor (seletor: string) {
+    constructor (seletor: string, escapar?: boolean) {
         this.elemento = document.querySelector(seletor);
+        escapar? this.escapar = escapar: this.escapar = false;
     }
 
     update(model: T):void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template
+                .replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.elemento.innerHTML = template;
     }
 
